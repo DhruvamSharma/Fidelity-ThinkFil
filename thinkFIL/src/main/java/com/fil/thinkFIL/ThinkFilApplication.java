a29b86962ac94a2fda80dc150858f4adbebdb59c
paloju <palojuvasudev05@gmail.com>
2018-07-26 21:20:19 +0530

    first commit

---
+++ thinkFIL/src/main/java/com/fil/thinkFIL/ThinkFilApplication.java
@@ -0,0 +1,12 @@
+package com.fil.thinkFIL;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThinkFilApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThinkFilApplication.class, args);
+	}
+}
---
+++ thinkFIL/src/main/java/com/fil/thinkFIL/controllers/HelloController.java
@@ -0,0 +1,15 @@
+package com.fil.thinkFIL.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+
+public class HelloController {
+	
+	@RequestMapping("/hello")
+	public String index() {
+		System.out.println("hello");
+		return "hello";
+	}
+}
