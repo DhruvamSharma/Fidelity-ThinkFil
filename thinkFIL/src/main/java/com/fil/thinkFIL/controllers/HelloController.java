9c160f5718022b42591c4148116775994c340501
paloju <palojuvasudev05@gmail.com>
2018-07-26 21:35:40 +0530

    branch push testing

--- thinkFIL/src/main/java/com/fil/thinkFIL/controllers/HelloController.java
+++ thinkFIL/src/main/java/com/fil/thinkFIL/controllers/HelloController.java
@@ -2,6 +2,7 @@ package com.fil.thinkFIL.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 
@@ -11,5 +12,11 @@ public class HelloController {
 	public String index() {
 		System.out.println("hello");
 		return "hello";
+	}
+	
+	@RequestMapping("/hello2")
+	@ResponseBody
+	public String index2() {
+		return "hello world from second route";
 	}
 }
