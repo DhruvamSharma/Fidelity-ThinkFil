3e26aebea4b0611f0999bcba5bf86562faa2753e
paloju vasu dev <palojuvasudev05@gmail.com>
2018-07-27 14:21:48 +0530

    login and signup controllers done

--- thinkFIL/src/main/java/com/fil/thinkFIL/controllers/UserController.java
+++ thinkFIL/src/main/java/com/fil/thinkFIL/controllers/UserController.java
@@ -1,5 +1,20 @@
 package com.fil.thinkFIL.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class UserController {
+	
+	@GetMapping(path="/login")
+	public String getLoginPage() {
+		System.out.println("login route found");
+		return "/login/login";
+	}
+	
+	@GetMapping(path="/signup")
+	public String getSignupPage() {
+		return "/signup/signup";
+	}
 
 }
