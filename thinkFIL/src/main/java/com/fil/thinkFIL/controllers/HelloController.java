d23b4e60ac251752ddc5185f09541a5f3f7a8f2b
Dhruvam <dhruvamssharma@gmail.com>
2018-07-27 11:10:16 +0530

    created a branch and folders

--- thinkFIL/src/main/java/com/fil/thinkFIL/controllers/HelloController.java
+++ thinkFIL/src/main/java/com/fil/thinkFIL/controllers/HelloController.java
@@ -2,10 +2,8 @@ package com.fil.thinkFIL.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
-
 public class HelloController {
 	
 	@RequestMapping("/hello")
